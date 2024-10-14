6def3a9babf375d4144e99f027bd06dd28eba881
Ted Kaczynski <bro@moment.net>
2024-10-14 17:47:23 -0600

    WIP (task2)

--- Makefile
+++ Makefile
@@ -182,6 +182,7 @@ UPROGS=\
 	_wc\
 	_zombie\
 	_ps\
+	_nice\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
---
+++ nice.c
@@ -0,0 +1,17 @@
+#define DEF_P 20
+
+int main(int argc, char *argv[]){
+    int
+        n   = DEF_P,
+        pid = 0;
+
+    if (argc < 2) return -1;
+
+    pid = atoi( argv[1] );
+
+    if (argc > 2) n = atoi( argv[2] );
+
+    set_priority(pid, n);
+
+    return 0;
+}
