1beedd4edb67c0ec8a026db2c20114cb8ba69607
Ted Kaczynski <bro@moment.net>
2024-10-15 22:45:57 -0600

    debugging

--- proc.c
+++ proc.c
@@ -333,7 +333,7 @@ scheduler(void)
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+    for(p = high = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
 
@@ -343,6 +343,7 @@ scheduler(void)
 
     p = high;
 
+    if( p->state == RUNNABLE ){
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
@@ -356,6 +357,7 @@ scheduler(void)
       // Process is done running for now.
       // It should have changed its p->state before coming back.
       c->proc = 0;
+    }
     release(&ptable.lock);
 
   }
